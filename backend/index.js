7739083d5f0f0d3c3d7984523947cb6a2daf19ea
Oleksandr <oleksandrptuha1@gmail.com>
2023-11-23 20:48:23 +0200

    created frontend page and added tailwindcss

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,30 @@
 import express from "express";
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js'
+import { Book } from './models/bookModel.js';
 import { PORT, mongoDBURL } from "./config.js";
-import booksRoute from "./routes/booksRoute.js"
+import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//middleware for routes
+app.use('/books', booksRoute)
+
+//Middleware for CORS
+app.use(cors({
+    origin: 'https://localhost:3000',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 //Root route
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send('welcome to mern stack tutorial')
 });
 
-app.use('/books', booksRoute)
+
 
 mongoose.connect(mongoDBURL)
     .then(()=>{
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-400 text-white">
+        <h1 className="text-3xl font-bold">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
